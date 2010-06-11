0b2408ab660086a8a0ecdb619b92a6ac424c8595
unknown <Gregor@.(none)>
2010-06-11 23:01:28 +0200

    Moved away from specific fixtures to handle everything with definitions from the test pages.

--- fixtures/SICP.js
+++ fixtures/SICP.js
@@ -1,48 +1,6 @@
 var SICP = (function() {
 	var constructor = function () {};
 	var self = {
-		square: function (x) {
-			return x*x;
-		},
-		
-		abs: function (x) {
-			if (x < 0) {
-				return -x;
-			} else {
-				return x;
-			}
-		},
-		
-		average: function (x, y) {
-			return (x+y)/2;
-		},
-		
-		fixedPoint: function (f, start, tolerance) {
-			var depth = 0;
-			function closeEnough (x, y) {
-				return (self.abs(x - y) < tolerance);
-			};
-			function iter (oldVal, newVal) {
-				if (depth++ > 100) return newVal;
-				return (closeEnough(oldVal, newVal)) ? newVal : iter(newVal, f(newVal));
-			};
-			return iter(start, f(start));
-		},
-
-		restrict: function (func, position, value) {
-			return function() {
-				var args = [];
-				for (var i=0; i < position - 1; i++) {
-					args.push(arguments[i]);
-				};
-				args.push(value);
-				for (var i = position - 1; i < arguments.length; i++) {
-					args.push(arguments[i]);
-				};
-				return func.apply(this, args);
-			};
-		},
-		
 		createDecistionTableFixture: function (constructor) {
 			constructor.prototype = {
 				setX: function (x) {
@@ -68,5 +26,6 @@ var SICP = (function() {
 	constructor.prototype = self;
 	var SICP = new constructor();
 	SICP.constructor = constructor;
+	SICP.namespace = {};
 	return SICP;
 })();
--- fixtures/SICP/FunctionStore.js
+++ fixtures/SICP/FunctionStore.js
@@ -1,19 +1,39 @@
 SICP.FunctionStore = (function () {
 	var self = function () {
-		this.funcs = [];
-		with (this) {
-			this.defineAs = function(name, func) {
-				this[name] = eval("(" + func + ")");
-				this.funcs.push(name);
+		var isPrintEnabled = false;
+		with (SICP.namespace) {
+			this.evaluate = function (expression) {
+				return eval("(" + expression + ")");
 			}
 		}
-		this.resetDefinitions = function() {
-			for (var i in this.funcs) {
-				delete this[this.funcs[i]];
-			}
-			this.funcs = [];
+		this.defineAs = function (name, expression) {
+			this.log(name + " = " + expression + "\n");
+			SICP.namespace[name] = this.evaluate(expression);
 			return true;
 		}
+		this.deleteDefinitionOf = function (name) {
+			if ('undefined' == typeof SICP.namespace[name]) {
+				return false;
+			}
+			this.log("Deleting: " + name + ": " + SICP.namespace[name] + "\n");
+			delete SICP.namespace[name];
+			return true;
+		}
+		this.cleanNamespace = function () {
+			for (name in SICP.namespace) {
+				this.deleteDefinitionOf(name);
+			}
+			return true;
+		}
+		this.log = function (string) {
+			if (isPrintEnabled) {
+				print(string);
+			}
+		}
+		this.setPrintEnabled = function (enable) {
+			isPrintEnabled = JsSlim.Converter.toBool(enable);
+		}
 	};
-	return JsSlim.extend(SICP.constructor, self);
+	constructor = JsSlim.extend(SICP.constructor, self);
+	return constructor;
 })();
