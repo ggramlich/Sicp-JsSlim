2b5f53fe69c82ecdbfe4ae5780f48159c6a57613
unknown <Gregor@.(none)>
2010-06-18 12:55:33 +0200

    Added some more explanations and elaborated on the SicpWithSlim part.

--- fixtures/SICP/FunctionStore.js
+++ fixtures/SICP/FunctionStore.js
@@ -14,18 +14,28 @@ SICP.FunctionStore = (function () {
 			return true;
 		}
 		this.deleteDefinitionOf = function (name) {
-			if ('undefined' == typeof SICP.namespace[name]) {
+			if (!this.isDefined(name)) {
 				return false;
 			}
 			this.log("Deleting: " + name + ": " + SICP.namespace[name]);
 			delete SICP.namespace[name];
 			return true;
 		}
+		this.isDefined = function (name) {
+			return 'undefined' !== typeof SICP.namespace[name];
+		}
 		this.cleanNamespace = function () {
 			for (name in SICP.namespace) {
 				this.deleteDefinitionOf(name);
 			}
 			return true;
+		}
+		this.defineEvaluate = function () {
+			SICP.namespace["evaluate"] = this.evaluate;
+			return true;
+		}
+		this.evaluateOn = function (name) {
+			return this.evaluate(name).apply(this, [].splice.call(arguments, 1));
 		}
 	};
 	constructor = JsSlim.extend(SICP.constructor, self);
